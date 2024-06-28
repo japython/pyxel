179b3ca874ce0f2ccc58c3b5c8932cfc39934491
Iida <tiida767555@gmail.com>
2024-06-29 08:36:57 +0900

    test

--- python/pyxel/examples/learningbox/ninja.py
+++ python/pyxel/examples/learningbox/ninja.py
@@ -14,11 +14,7 @@ dy = 0
 pldir = 1
 #player direction status
 
-<<<<<<< HEAD
 #chkpoint = [(0,0),(8,0),(0,8),(8,8)]
-=======
-chkpoint = [(2,0),(6,0),(2,7),(6,7)]
->>>>>>> aca81296fa8bc831c502be4ae878c9e1ebbb255f
 
 #def chkwall(cx,cy):
     #c = 0
@@ -54,7 +50,7 @@ def update():
     # I cannot understand why dx and dy are necessary besides x and y though, it is easier to maintain and controle movement with flexibility.
 
     
-<<<<<<< HEAD
+
     #lr = pyxel.sgn(dx)
     
     #loop = abs(dx)
@@ -64,18 +60,7 @@ def update():
         #    break
         #x = x + lr
         #loop = loop -1
-=======
-    lr = pyxel.sgn(dx)
-    #Returns 1 when x is positive, 0 when it is zero, and -1 when it is negative
-    loop = abs(dx)
-    #
-    while 0 < loop :
-        if chkwall( x + lr, y) != 0:
-            dx = 0
-            break
-        x = x + lr
-        loop = loop -1
->>>>>>> aca81296fa8bc831c502be4ae878c9e1ebbb255f
+
     
     return
 
